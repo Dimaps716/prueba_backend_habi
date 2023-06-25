5afbe612a3d6dfafd04bf2e509c1af02f103ca58
dima <dimaps716@gmail.com>
2023-06-25 17:36:07 -0500

    tests test_search_properties_with_multiple_values_in_filters

--- src/tests/search_properties.py
+++ src/tests/search_properties.py
@@ -32,3 +32,8 @@ def test_search_properties_with_multiple_filters():
     response = client.get("/property?city=bogota&year=2011")
     assert response.status_code == 200
     assert len(response.json()) > 0
+
+def test_search_properties_with_multiple_values_in_filters():
+    response = client.get("/property?city=medellin&city=bogota&address=calle%2023%20%2345-67r&address=calle%2023%20%2345-67q")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
